eb346eec464ee72dbea8b91f1820561c7d292b51
Trungnq7474 <nguyenquangtrung0704@gmail.com>
2026-08-28 21:53:06 +0700

    Update database

--- config/db.js
+++ config/db.js
@@ -9,10 +9,10 @@
 const sql = require('mssql');
 
 const config = {
-    user: 'sa',
-    password: '0352493970',
-    server: 'localhost',
-    database: 'shopbida',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
     options: {
         trustServerCertificate: true
     }
