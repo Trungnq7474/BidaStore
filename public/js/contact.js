fddbde144faebb681b04c81411a207dd074b373b
Trungnq7474 <nguyenquangtrung0704@gmail.com>
2026-09-02 00:08:02 +0700

    update contacts

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,19 +1,35 @@
-document.querySelector('.send').addEventListener('click', async () => {
-
-    const name = document.querySelector('.name').value;
-    const email = document.querySelector('.email').value;
-    const mess = document.querySelector('.mess').value;
-    const phone = document.querySelector('.phone').value;
+async function getUser() {
 
     const resUser = await fetch('/get-user');
     const dataUser = await resUser.json();
 
+    if (!dataUser.user) {
+        return;
+    }
+
+    document.querySelector('.name').value = dataUser.user.username;
+    document.querySelector('.email').value = dataUser.user.email;
+    document.querySelector('.phone').value = dataUser.user.phone;
+}
+
+getUser();
+
+document.querySelector('.send').addEventListener('click', async () => {
+
+    const resUser = await fetch('/get-user');
+    const dataUser = await resUser.json();
+  
     if(!dataUser.user) {
         show("Bạn Vui Lòng Đăng Nhập Để Sử Dụng Chức Năng Này !");
         return;
     }
 
-    if(name == "" || email == "" || mess == "" || phone === "")  {
+    const name = document.querySelector('.name').value.trim();
+    const email = document.querySelector('.email').value.trim();
+    const phone = document.querySelector('.phone').value.trim();
+    const mess = document.querySelector('.mess').value.trim();
+
+    if (!name || !email || !mess || !phone) {
         show("Bạn Không Được Phép Để Trống !");
         return;
     }
@@ -24,10 +40,10 @@ document.querySelector('.send').addEventListener('click', async () => {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ 
-            name, 
-            email, 
-            mess,
-            phone
+            name: name, 
+            email: email, 
+            mess: mess,
+            phone: phone
         })
     });
 
@@ -36,10 +52,8 @@ document.querySelector('.send').addEventListener('click', async () => {
     if(data =="ok") {
         show("Bạn Đã Gửi Liên Hệ Thành Công !");
 
-        document.querySelector('.name').value = "";
-        document.querySelector('.email').value = "";
         document.querySelector('.mess').value = "";
-        document.querySelector('.phone').value = "";
+       
     }
 });
 
