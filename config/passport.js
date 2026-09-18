540e900f7b1af63ba70686ccb0268f4a86d9782f
Trungnq7474 <nguyenquangtrung0704@gmail.com>
2026-09-18 22:26:38 +0700

    add register google

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const { sql } = require('./db');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || '/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         const email = profile.emails[0].value;
